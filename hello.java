53b1457c811a282e9476c868ffa42df7dc852923
Darin Joshua <dj2000official@gmail.com>
2019-08-25 16:18:12 +0530

    using Vscode for writing java commit of java code

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class hello
+{
+    public static void main(String args[])
+    {
+    System.out.println("Hello World");
+    String a="Josh";
+    String string = "Welcome back\t";
+    System.out.println(string +a);
+    }
+}
